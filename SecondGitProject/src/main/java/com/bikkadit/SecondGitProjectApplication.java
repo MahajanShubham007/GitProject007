daf5c00e89d1934b2542321eb5dec16e461df163
MahajanShubham007 <shubhammahajan16280@gmail.com>
2022-11-07 13:36:38 +0530

    this is second project of git

---
+++ SecondGitProject/src/main/java/com/bikkadit/SecondGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondGitProjectApplication.class, args);
+	}
+
+}
